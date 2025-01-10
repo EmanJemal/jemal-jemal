ce1b06e0dd489a0e5aec2deedb6df891b9df4899
Arafat Mohammed <arafatmohammedbusi@gmail.com>
2025-01-10 22:42:33 +0300

    header link check up

--- Scripts/Shared/header.js
+++ Scripts/Shared/header.js
@@ -1,6 +1,6 @@
 function checkWidth() {
     if (window.innerWidth < 960) {
-        // Call your function here
+        // Update icon to make it more mobile-friendly
         responsiveIcon();
     }
 }
@@ -8,14 +8,18 @@ function checkWidth() {
 function responsiveIcon() {
     const classRoom = document.querySelector('.classroom-link');
     classRoom.innerHTML = `<i class="fa-solid fa-house"></i>`;
+    
     const Assignment = document.querySelector('.classroom-assignment');
     Assignment.innerHTML = `<i class="fa-solid fa-book"></i>`;
+    
     const rooms = document.querySelector('.classroom-rooms');
     rooms.innerHTML = `<i class="fa-solid fa-person-booth"></i>`;
+    
     const settings = document.querySelector('.classroom-setting');
-    settings.innerHTML =  `<i class="fa-solid fa-gear"></i>`;
+    settings.innerHTML = `<i class="fa-solid fa-gear"></i>`;
+    
     const profile = document.querySelector(".classroom-profile");
-    profile.innerHTML =  `<i class="fa-solid fa-user"></i>`;
+    profile.innerHTML = `<i class="fa-solid fa-user"></i>`;
 };
 
 // Add event listener for resize to check width on window resize
@@ -23,5 +27,3 @@ window.addEventListener('resize', checkWidth);
 
 // Initial check
 checkWidth();
-
-
